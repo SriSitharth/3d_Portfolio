8193c910b610a20bf15a130f923b5450e59cb5aa
Sri Sitharth <srisitharth1998@gmail.com>
2025-10-11 15:48:32 +0530

    new file:   CNAME

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/3D_Portfolio/",
+  base: "/3d_Portfolio/",
 });
