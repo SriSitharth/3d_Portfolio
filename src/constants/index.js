62e63a3129d0f09931b2b1141cd6b0d38cf6c86d
Sri Sitharth <srisitharth1998@gmail.com>
2025-10-11 15:03:42 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,376 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "Web Development",
+    description:
+      "I specialize in creating responsive and visually appealing websites using the latest web technologies.",
+    subDescription: [
+      "Built responsive web applications using HTML, CSS, JavaScript, and frameworks such as React.js, jQuery, and Bootstrap.",
+      "Implemented authentication using Google OAuth and Firebase Auth.",
+      "Integrated CRUD operations with Firestore for real-time updates.",
+      "Enhanced performance using lazy loading and code-splitting.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/web.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 2,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 3,
+        name: "JavaScript",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 4,
+        name: "React.js",
+        path: "/assets/logos/react.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Mobile App Development",
+    description:
+      "I develop high-performance mobile applications using Flutter, ensuring seamless integration with Firebase for real-time data management.",
+    subDescription: [
+      "My apps are designed to provide an intuitive user experience on both iOS and Android platforms.",
+      "Developed cross-platform mobile apps using Flutter and Firebase.",
+      "Designed reusable UI components with Tailwind-inspired styling for Flutter.",
+      "Added role-based access control and secure data synchronization.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/app.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Flutter",
+        path: "/assets/logos/auth0.svg",
+      },
+      {
+        id: 2,
+        name: "Dart",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 3,
+        name: "Firebase",
+        path: "/assets/logos/sqlite.svg",
+      },
+      {
+        id: 4,
+        name: "Git",
+        path: "/assets/logos/git.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "UI/UX Design",
+    description:
+      "I design user-friendly and visually stunning interfaces using Figma.",
+    subDescription: [
+      "My focus is on creating designs that not only look great but also enhance the user experience by aligning with client requirements and user expectations.",
+      "Designed clean, modern interfaces in Figma based on client requirements.",
+      "Created interactive prototypes for better visualization and feedback.",
+      "Converted Figma designs into responsive React components using Tailwind CSS.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/ui.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Figma",
+        path: "/assets/logos/blazor.svg",
+      },
+      {
+        id: 2,
+        name: "Wix",
+        path: "/assets/logos/dotnetcore.svg",
+      },
+      {
+        id: 3,
+        name: "Spline",
+        path: "/assets/logos/sqlite.svg",
+      },
+      {
+        id: 4,
+        name: "Photoshop",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Custom AI Solutions",
+    description:
+      "I develop custom chatbots using latest GPT for seamless customer service and user engagement.",
+    subDescription: [
+      "Built AI-powered floor plan generator using OpenAI DALL·E 3 API.",
+      "Automated content generation and image processing using Python scripts.",
+      "Integrated GPT models for intelligent chatbot functionality.",
+      "Optimized model responses with prompt engineering techniques.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/ai.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Python",
+        path: "/assets/logos/cplusplus.svg",
+      },
+      {
+        id: 2,
+        name: "C#",
+        path: "/assets/logos/csharp.svg",
+      },
+      {
+        id: 3,
+        name: "Git",
+        path: "/assets/logos/git.svg",
+      },
+      {
+        id: 4,
+        name: "OpenAI",
+        path: "/assets/logos/microsoft.svg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Automation Solutions",
+    description:
+      "I develop automation scripts using Python to streamline repetitive tasks and improve efficiency.",
+    subDescription: [
+      "From web page editing automation to white space detection systems using OpenCV, my solutions save time and reduce manual effort.",
+      "Created automation tools using Python and OpenCV for image-based tasks.",
+      "Developed scripts for detecting whitespace and place annotation in revit.",
+      "Implemented custom web hosting from XAMPP Server in Systems.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/software.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Python",
+        path: "/assets/logos/wordpress.svg",
+      },
+      {
+        id: 2,
+        name: "OpenCV",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 3,
+        name: "Node.js",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 4,
+        name: "Github",
+        path: "/assets/logos/github.svg",
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "3D Web Development",
+    description:
+      "Using Three.js, I create immersive websites featuring 3D models and point clouds.",
+    subDescription: [
+      "I incorporate interactive features to provide a unique and engaging user experience, making your website stand out.",
+      "Developed interactive 3D web apps using Three.js and React Three Fiber.",
+      "Added a quiz system with dynamic question generation and real-time grading.",
+      "Integrated Stripe API for secure payment processing.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/game-engine.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Three.js",
+        path: "/assets/logos/threejs.svg",
+      },
+      {
+        id: 2,
+        name: "GSAP",
+        path: "/assets/logos/azure.svg",
+      },
+      {
+        id: 3,
+        name: "Potree",
+        path: "/assets/logos/stripe.svg",
+      },
+      {
+        id: 4,
+        name: "Blender",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 7,
+    title: "Revit API Development",
+    description:
+      "A web application that allows users to enroll in courses, watch video lectures, and take quizzes.",
+    subDescription: [
+      "Built using Blazor WebAssembly for a seamless SPA experience.",
+      "Implemented video streaming with Azure Media Services.",
+      "Added a quiz system with dynamic question generation and real-time grading.",
+      "Integrated Stripe API for secure payment processing.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/revit.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "C#",
+        path: "/assets/logos/csharp.svg",
+      },
+      {
+        id: 2,
+        name: ".Net",
+        path: "/assets/logos/dotnet.svg",
+      },
+      {
+        id: 3,
+        name: "Revit API",
+        path: "/assets/logos/stripe.svg",
+      },
+      {
+        id: 4,
+        name: "Forge API",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "https://wa.me/+919487755171",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/srisithartht/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/sri_sitharth/",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Full Stack Developer",
+    job: "Addinstech LLP",
+    date: "Jun 24 - Present",
+    contents: [
+      "✅ Developed a 3D shortest path finding tool using the A* algorithm with Revit API (C#), Three.js and WPF for user interface.",
+      "✅ Integrated Autodesk Forge Viewer with Design Automation API to dynamically apply wall finishes in 3D Revit models.",
+      "✅ Designed a web application using the MERN stack and the OpenAI DALL-E 3 API, with Google OAuth for user authentication.",
+      "✅ Built an inventory management app for a manufacturing company using Electron for windows, integrated with React.js, Ant Design (AntD), and Firebase for efficient product tracking and management.",
+    ],
+  },
+  {
+    title: "Full Stack Developer",
+    job: "Qosteq IT Private Limited",
+    date: "Apr 23 - May 24",
+    contents: [
+      "✅ Built many responsive websites using HTML, CSS, JavaScript and frameworks like React.js, jQuery, Bootstrap and created prototypes in Figma to meet client requirements.",
+      "✅ Developed two Flutter applications integrated with Firebase database for internal workflow/office use and real-time data management.",
+      "✅ Developed a Python script to automate the process of editing webpage and hosted it on the XAMPP server.",
+      "✅ Created a white space detection system using OpenCV and Python to automatically place tags in Revit for identified spaces and achieving significant progress.",
+      "✅ Designed and developed a forge viewer-based website featuring 3D models and point clouds using Three.js (Potree) and implemented some interactive features.",
+    ],
+  },
+  {
+    title: "Senior Associate",
+    job: "Guidehouse India Private Limited",
+    date: "Sep 21 - Mar 23",
+    contents: [
+      "✅ Maintained 99% accuracy in entering customer records.",
+      "✅ Worked accurately under pressure to meet competing deadlines.",
+      "✅ Improved typing speed through regular practice and training sessions.",
+      "✅ Ensured confidentiality and compliance by handling sensitive customer information securely.",
+    ],
+  },
+  {
+    title: "Customer Support Specialist",
+    job: "B2C Technologies Private Limited",
+    date: "Feb 20 - Jul 20",
+    contents: [
+      "✅ Handling 300+ customer interactions per day.",
+      "✅ Inbound and Outbound call handling - (Night Shift).",
+      "✅ Stay calm at all times, even during moments of customer displeasure.",
+    ],
+  },
+];
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
---
+++ src/sections/Experiences.jsx
@@ -0,0 +1,13 @@
+import { Timeline } from "../components/Timeline";
+import { experiences } from "../constants";
+const Experiences = () => {
+  return (
+    <section id="work">
+    <div className="w-full">
+      <Timeline data={experiences} />
+    </div>
+    </section>
+  );
+};
+
+export default Experiences;
